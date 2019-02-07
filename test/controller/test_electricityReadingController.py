11783bad138b16279de1a6f4cd09ae442ef73bb3
Martin Soto <donsoto@gmail.com>
2019-02-07 17:52:38 +0100

    Add tests for error cases of reading controller

--- test/controller/test_electricityReadingController.py
+++ test/controller/test_electricityReadingController.py
@@ -44,3 +44,21 @@ class MyTestCase(unittest.TestCase):
         self.assertIn({"time": 1505825838, "reading": 0.6 }, readings)
         self.assertIn({"time": 1505825848, "reading": 0.65 }, readings)
         self.assertIn({"time": 1605825849, "reading": 0.7}, readings)
+
+    def test_respond_with_error_if_smart_meter_id_not_set(self):
+        readingJson = {
+            "electricityReadings": [
+                { "time": 1505825838, "reading": 0.6 }
+            ]
+        }
+
+        with self.assertRaises(Exception):
+            self.client.post('/readings/store', json=readingJson)
+
+    def test_respond_with_error_if_electricity_readings_not_set(self):
+        readingJson = {
+            "smartMeterId": "meter-11"
+        }
+
+        with self.assertRaises(Exception):
+            self.client.post('/readings/store', json=readingJson)
