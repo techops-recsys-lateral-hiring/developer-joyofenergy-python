49e42828c1d0419a212a86b7e353b98b3c5aa1c7
Martin Soto <donsoto@gmail.com>
2019-02-07 18:56:09 +0100

    New PricePlan class

---
+++ src/domain/price_plan.py
@@ -0,0 +1,25 @@
+class PricePlan:
+    def __init__(self, name, supplier, unit_rate, peak_time_multipliers):
+        self.name = name
+        self.supplier = supplier
+        self.unit_rate = unit_rate
+        self.peak_time_multipliers = peak_time_multipliers
+
+    def get_price(self, date_time):
+        matching_multipliers = [m for m in self.peak_time_multipliers
+                                if m.day_of_week == date_time.isoweekday()]
+        return self.unit_rate * matching_multipliers[0].multiplier if len(matching_multipliers) else self.unit_rate
+
+    class DayOfWeek:
+        SUNDAY = 0
+        MONDAY = 1
+        TUESDAY = 2
+        WEDNESDAY = 3
+        THUESDAY = 4
+        FRIDAY = 5
+        SATURDAY = 6
+
+    class PeakTimeMultiplier:
+        def __init__(self, day_of_week, multiplier):
+            self.day_of_week = day_of_week
+            self.multiplier = multiplier
---
+++ test/domain/test_PricePlan.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from unittest import TestCase
+
+from domain.price_plan import PricePlan
+
+
+class TestPricePlan(TestCase):
+    def test_return_the_base_price_given_an_off_peak_date_time(self):
+        peak_time_multiplier = PricePlan.PeakTimeMultiplier(PricePlan.DayOfWeek.WEDNESDAY, 10)
+        off_peak_time = datetime(2000, 1, 1, 11, 11, 11)
+
+        plan = PricePlan('plan-name', 'supplier-name', 1, [ peak_time_multiplier ])
+
+        price = plan.get_price(off_peak_time)
+
+        self.assertEqual(price, 1)
+
+    def test_return_a_peak_price_given_a_datetime_matching_peak_day(self):
+        peak_time_multiplier = PricePlan.PeakTimeMultiplier(PricePlan.DayOfWeek.WEDNESDAY, 10)
+        off_peak_time = datetime(2000, 1, 5, 11, 11, 11)
+
+        plan = PricePlan('plan-name', 'supplier-name', 1, [ peak_time_multiplier ])
+
+        price = plan.get_price(off_peak_time)
+
+        self.assertEqual(price, 10)
