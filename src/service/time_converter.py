e1ef6b79716b24bf6f70f77a9caf43ddc952d5d8
Martin Soto <donsoto@gmail.com>
2019-02-10 17:30:51 +0100

    Use plain Unix timestamps for time converter

--- src/service/time_converter.py
+++ src/service/time_converter.py
@@ -2,5 +2,5 @@ from datetime import datetime
 
 class TimeConverter:
     @staticmethod
-    def time_elapsed_in_hours(earliest_datetime, latest_datetime):
-        return (latest_datetime - earliest_datetime).total_seconds() / 3600
+    def time_elapsed_in_hours(earliest_unix_timestamp, latest_unix_timestamp):
+        return (latest_unix_timestamp - earliest_unix_timestamp) / 3600
--- test/service/test_TimeConverter.py
+++ test/service/test_TimeConverter.py
@@ -3,10 +3,12 @@ from unittest import TestCase
 
 from service.time_converter import TimeConverter
 
-
 class TestTimeConverter(TestCase):
     def test_calculate_elapsed_time_in_hours_from_two_unix_timestamps(self):
-        earlier = datetime.fromisoformat('2018-05-24T11:30:00')
-        later = datetime.fromisoformat('2018-05-24T12:00:00')
+        earlier = self.unix_time_of(datetime.fromisoformat('2018-05-24T11:30:00'))
+        later = self.unix_time_of(datetime.fromisoformat('2018-05-24T12:00:00'))
 
         self.assertEqual(TimeConverter.time_elapsed_in_hours(earlier, later), 0.5)
+
+    def unix_time_of(self, dt):
+        return (dt - datetime(1970,1,1)).total_seconds()
