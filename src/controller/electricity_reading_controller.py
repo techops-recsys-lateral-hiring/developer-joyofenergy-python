312d598f9e1f2bee2b1a38f90b5f570fab436b49
Martin Soto <donsoto@gmail.com>
2019-02-10 20:30:40 +0100

    Use reading service from controller

--- src/controller/electricity_reading_controller.py
+++ src/controller/electricity_reading_controller.py
@@ -1,17 +1,19 @@
 from flask import abort
 
 from repository.electricity_reading_repository import ElectricityReadingRepository
+from service.electricity_reading_service import ElectricityReadingService
 
 repository = ElectricityReadingRepository()
+service = ElectricityReadingService(repository)
 
 
 def store(data):
-    repository.store(data['smartMeterId'], data['electricityReadings'])
+    service.store_reading(data)
     return data
 
 
 def read(smart_meter_id):
-    readings = repository.find(smart_meter_id)
+    readings = service.retrieve_readings_for(smart_meter_id)
     if len(readings) < 1:
         abort(404)
     else:
