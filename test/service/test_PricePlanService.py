9ab375297482455fa5e4a55a195ec6558726eb5f
Nina Hartmann <nhartman@thoughtworks.com>
2020-03-02 13:00:54 +0100

    Use some easier to understand test data.

--- test/service/test_PricePlanService.py
+++ test/service/test_PricePlanService.py
@@ -6,6 +6,7 @@ from domain.price_plan import PricePlan
 from repository.electricity_reading_repository import ElectricityReadingRepository
 from repository.price_plan_repository import price_plan_repository
 from service.price_plan_service import PricePlanService
+from service.time_converter import TimeConverter
 
 class TestPricePlanService(TestCase):
     electricity_reading_repository = ElectricityReadingRepository()
@@ -20,13 +21,13 @@ class TestPricePlanService(TestCase):
             PricePlan('X6', 'XS6', 1, [])])
 
         reading_service_mock = MagicMock(return_value=[
-            ElectricityReading({ "time": 1510307115, "reading": 0.5778640126312636 }),
-            ElectricityReading({ "time": 1510307125, "reading": 0.19979840426464207 }),
-            ElectricityReading({ "time": 1510307135, "reading": 0.22644598149484024 })
+            ElectricityReading({ "time": TimeConverter.iso_format_to_unix_time('2017-11-10T09:00:00'), "reading": 0.65 }),
+            ElectricityReading({ "time": TimeConverter.iso_format_to_unix_time('2017-11-10T09:30:00'), "reading": 0.35 }),
+            ElectricityReading({ "time": TimeConverter.iso_format_to_unix_time('2017-11-10T10:00:00'), "reading": 0.5 })
         ])
         self.price_plan_service.electricity_reading_service.retrieve_readings_for = reading_service_mock
 
         spend = self.price_plan_service.get_list_of_spend_against_each_price_plan_for('smart-meter-1001')
-        self.assertEqual(spend[0], { 'X6': 60.24650390344476 })
-        self.assertEqual(spend[1], { 'X2': 120.49300780688952 })
-        self.assertEqual(spend[2], { 'X1': 602.4650390344476 })
+        self.assertEqual(spend[0], { 'X6': 0.5 })
+        self.assertEqual(spend[1], { 'X2': 1 })
+        self.assertEqual(spend[2], { 'X1': 5 })
